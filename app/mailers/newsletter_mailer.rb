3bb454c86b65e2edd097353fa362dc6209880d0c
craigmd <craigmichaeldickson@gmail.com>
2016-03-22 16:11:22 -0400

    Email styles first draft

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,16 +1,27 @@
 class NewsletterMailer < ActionMailer::Base
-  default from: "#{ENV['FROM_NAME']} <#{ENV['FROM_EMAIL']}>"
-
+  # default from: "#{ENV['FROM_NAME']} <#{ENV['FROM_EMAIL']}>"
+  default from: "craig@gmail.com"
   add_template_helper(ApplicationHelper)
 
-  def mail_newsletter(contact_list, newsletter = Newsletter.current)
+  # def mail_newsletter(contact_list, newsletter = Newsletter.current)
+  #   @newsletter = newsletter
+  #   @articles = @newsletter.articles_to_send.map { |article_id| Article.find(article_id) }
+  #   puts "Sending emails."
+  #   contact_emails = contact_list.map { |contact| contact.email }
+  #   mail(
+  #     to: ENV['FROM_EMAIL'],
+  #     bcc: contact_emails,
+  #     subject: "#{@newsletter.title} - #{@newsletter.newsletter_date}",
+  #     template_name: 'newsletter'
+  #   )
+  # end
+
+  def mail_newsletter(newsletter = Newsletter.current)
     @newsletter = newsletter
     @articles = @newsletter.articles_to_send.map { |article_id| Article.find(article_id) }
     puts "Sending emails."
-    contact_emails = contact_list.map { |contact| contact.email }
     mail(
-      to: ENV['FROM_EMAIL'],
-      bcc: contact_emails,
+      to: "dcn@dcn.com",
       subject: "#{@newsletter.title} - #{@newsletter.newsletter_date}",
       template_name: 'newsletter'
     )
