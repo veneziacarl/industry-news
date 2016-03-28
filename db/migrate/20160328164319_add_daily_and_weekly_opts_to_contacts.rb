33c454fa456709d557db6143503cd7bff72387eb
Carl Venezia <veneziacarl@gmail.com>
2016-03-28 13:40:54 -0400

    updated migration to account for current data

--- db/migrate/20160328164319_add_daily_and_weekly_opts_to_contacts.rb
+++ db/migrate/20160328164319_add_daily_and_weekly_opts_to_contacts.rb
@@ -1,6 +1,15 @@
 class AddDailyAndWeeklyOptsToContacts < ActiveRecord::Migration
-  def change
+  def up
     add_column :contacts, :daily_news, :boolean, null: false, default: 'true'
     add_column :contacts, :weekly_news, :boolean, null: false, default: 'true'
+    Contact.find_each do |contact|
+      contact.daily_news = true
+      contact.weekly_news = true
+    end
+  end
+
+  def down
+    remove_column :contacts, :daily_news
+    remove_column :contacts, :weekly_news
   end
 end
