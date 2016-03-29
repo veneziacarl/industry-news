3fe2457b9eca90e86bfb517957e8160f6fbaa158
Carl Venezia <veneziacarl@gmail.com>
2016-03-28 22:01:22 -0400

    changed the subscription form to a native mailchimp form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317060550) do
+ActiveRecord::Schema.define(version: 20160328164319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,9 +34,11 @@ ActiveRecord::Schema.define(version: 20160317060550) do
   end
 
   create_table "contacts", force: :cascade do |t|
-    t.string   "email",      default: "", null: false
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.string   "email",       default: "",   null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "daily_news",  default: true, null: false
+    t.boolean  "weekly_news", default: true, null: false
   end
 
   add_index "contacts", ["email"], name: "index_contacts_on_email", unique: true, using: :btree
